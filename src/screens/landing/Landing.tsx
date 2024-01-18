892cc7e6b2ba31de707310afe5b54e2f8cc1d7c5
Victor Whyte <victordavidwhyte@gmail.com>
2024-01-18 08:40:00 +0100

    feat: convert why secure seed fund section

--- src/screens/landing/Landing.tsx
+++ src/screens/landing/Landing.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/sections/landing/HeroSection";
 import HowToInvest from "@/components/sections/landing/HowToInvest";
 import InvestSecurely from "@/components/sections/landing/InvestSecurely";
+import WhySecureSeedFund from "@/components/sections/landing/WhySecureSeedFund";
 import React, { Fragment } from "react";
 
 const Landing = () => {
@@ -9,6 +10,7 @@ const Landing = () => {
       <HeroSection />
       <InvestSecurely />
       <HowToInvest />
+      <WhySecureSeedFund />
     </Fragment>
   );
 };
