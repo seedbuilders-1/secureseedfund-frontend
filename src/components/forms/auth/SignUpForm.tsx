af9fa6e6e153f4eee3c7b0d0b85a4517061ecb4b
Abubakar-1 <abubakaruabdulkadir@gmail.com>
2024-08-10 15:40:48 +0100

    Auth Screens/final

--- src/components/forms/auth/SignUpForm.tsx
+++ src/components/forms/auth/SignUpForm.tsx
@@ -19,6 +19,11 @@ import useUserAuth from "@/hooks/auth/useAuth";
 import { useRouter } from "next/navigation";
 import { useToast } from "@/components/ui/use-toast";
 
+import facebook from "../../../../public/assets/icons/icons8-facebook.svg";
+import linkednin from "../../../../public/assets/icons/icons8-linkedin.svg";
+import google from "../../../../public/assets/icons/icons8-google.svg";
+import Image from "next/image";
+
 const SignUpForm = () => {
   const { loading, registerUser, Registered } = useUserAuth();
   const form = useForm<SignUpValidation>({
@@ -49,16 +54,16 @@ const SignUpForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-[400px]">
         <div className="mb-3 grid grid-cols-2 gap-x-2 gap-y-4">
           <FormField
             control={form.control}
             name="firstName"
             render={({ field }) => (
-              <FormItem className="col-span-2 md:col-span-1" >
-                <FormLabel>First Name</FormLabel>
+              <FormItem className="col-span-2">
+                {/* <FormLabel>First Name</FormLabel> */}
                 <FormControl>
-                  <Input placeholder="Ex. Jane" {...field} />
+                  <Input placeholder="First Name" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -68,10 +73,10 @@ const SignUpForm = () => {
             control={form.control}
             name="lastName"
             render={({ field }) => (
-              <FormItem className="col-span-2 md:col-span-1"> 
-                <FormLabel>Last Name</FormLabel>
+              <FormItem className="col-span-2">
+                {/* <FormLabel>Last Name</FormLabel> */}
                 <FormControl>
-                  <Input placeholder="Ex. Doe" {...field} />
+                  <Input placeholder="Last Name" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -82,9 +87,9 @@ const SignUpForm = () => {
             name="email"
             render={({ field }) => (
               <FormItem className="col-span-2">
-                <FormLabel>Email address</FormLabel>
+                {/* <FormLabel>Email address</FormLabel> */}
                 <FormControl>
-                  <Input placeholder="Ex. jane@example.com" {...field} />
+                  <Input placeholder="Email Address" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -95,13 +100,9 @@ const SignUpForm = () => {
             name="password"
             render={({ field }) => (
               <FormItem className="col-span-2">
-                <FormLabel>Password</FormLabel>
+                {/* <FormLabel>Password</FormLabel> */}
                 <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="Enter Password"
-                    {...field}
-                  />
+                  <Input type="password" placeholder="Password" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -112,11 +113,11 @@ const SignUpForm = () => {
             name="confirmPassword"
             render={({ field }) => (
               <FormItem className="col-span-2">
-                <FormLabel>Confirm Password</FormLabel>
+                {/* <FormLabel>Confirm Password</FormLabel> */}
                 <FormControl>
                   <Input
                     type="password"
-                    placeholder="Enter Confirm Password"
+                    placeholder="Confirm Password"
                     {...field}
                   />
                 </FormControl>
@@ -125,17 +126,21 @@ const SignUpForm = () => {
             )}
           />
         </div>
-        <Button type="submit" className="w-full rounded-md" loading={loading}>
+        <Button
+          type="submit"
+          className="w-full bg-[#241A3F] hover:bg-[#241A3F]/90 rounded-3xl"
+          loading={loading}
+        >
           Create account
         </Button>
-        <p className="text-[.8rem] text-center text-slate-700 leading-[1.25rem] mt-6">
-          Already have an account?{" "}
-          <Link href="/auth/sign-in">
-            <span className="font-[500] text-primaryMain hover:underline cursor-pointer">
-              Sign in
-            </span>
-          </Link>
-        </p>
+        {/* <p className="text-[.8rem] text-center text-slate-700 leading-[1.25rem] mt-6">
+              Already have an account?{" "}
+              <Link href="/auth/sign-in">
+                <span className="font-[500] text-primaryMain hover:underline cursor-pointer">
+                  Sign in
+                </span>
+              </Link>
+            </p> */}
       </form>
     </Form>
   );
