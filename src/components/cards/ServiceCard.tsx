ad13cf62d935c5ca5f98eb19146fdbb18839923d
Abubakar-1 <abubakaruabdulkadir@gmail.com>
2024-09-18 11:10:54 +0100

    PR changes

--- src/components/cards/ServiceCard.tsx
+++ src/components/cards/ServiceCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "../ui/button";
+import Image from "next/image";
 
 interface ServiceCardProps {
   imageSrc: string;
@@ -17,15 +18,16 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
   return (
     <div className="relative max-w-sm md:w-[20rem] rounded-2xl shadow-lg pb-12 border-[7px] border-[#00BD8F]">
       <div className="relative w-full h-64 overflow-hidden rounded-t-lg">
-        <img
+        {/* <img
           src={imageSrc}
           alt={title}
           className="object-cover w-full h-full"
-        />
+        /> */}
+        <Image src={imageSrc} alt={title} layout="fill" />
       </div>
 
       <div className="mt-4 text-center">
-        <h3 className="text-xl font-bold mb-2">{title}</h3>
+        <h3 className="text-xl font-bold mb-2 px-2">{title}</h3>
         <p className="text-2xl font-semibold">${price}</p>
       </div>
 
