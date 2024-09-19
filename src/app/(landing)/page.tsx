794aea683667b7852d9eaece79e4c69dd992536a
unknown <hassanjaeh@gmail.com>
2024-09-19 16:55:18 +0100

    landing pages conversion

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,5 @@
-import FAQSection from "@/components/sections/landing/FAQSection";
 import HeroSection from "@/components/sections/landing/HeroSection";
-import HowToInvest from "@/components/sections/landing/HowToInvest";
 import InvestSecurely from "@/components/sections/landing/InvestSecurely";
-import NewToInvesting from "@/components/sections/landing/NewToInvesting";
-import WhySecureSeedFund from "@/components/sections/landing/WhySecureSeedFund";
 import React, { Fragment } from "react";
 import { Metadata } from "next";
 import WhyInvest from "@/app/(landing)/home/components/WhyInvest";
