dc2459d556f3c067202fcebbce230b10b77e6bb4
hassan <hassan@hassans-MacBook-Pro.local>
2024-11-04 11:47:57 +0100

    ad

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,9 +2,9 @@
 
 import { ReactNode } from "react";
 import DashboardLayout from "@/components/layouts/DashboardLayout";
-import { RiHome5Line } from "react-icons/ri";
+import { RiHome5Line, RiWalletLine, RiUserLine } from "react-icons/ri";
 import { SiWpexplorer } from "react-icons/si";
-import { Wallet, User } from "lucide-react";
+import { Compass } from "lucide-react";
 import useUserAuth from "@/hooks/auth/useAuth";
 import { LoadingSpinner } from "@/components/shared/LoadingSpinner";
 import PrivateRoute from "@/route-helpers/PrivateRoute";
@@ -14,6 +14,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
   const role = user?.accountType;
 
   const items = [
+    // Startup routes
     {
       name: "Dashboard",
       icon: RiHome5Line,
@@ -28,16 +29,17 @@ const Layout = ({ children }: { children: ReactNode }) => {
     },
     {
       name: "Wallet",
-      icon: Wallet,
+      icon: RiWalletLine,
       path: `/dashboard/startup/wallet`,
       allowedRoles: ["startup"],
     },
     {
       name: "Account",
-      icon: User,
+      icon: RiUserLine,
       path: `/dashboard/startup/account`,
       allowedRoles: ["startup"],
     },
+    // Investor routes
     {
       name: "Dashboard",
       icon: RiHome5Line,
@@ -46,19 +48,19 @@ const Layout = ({ children }: { children: ReactNode }) => {
     },
     {
       name: "Explore",
-      icon: RiHome5Line,
+      icon: Compass,
       path: `/dashboard/investor/explore`,
       allowedRoles: ["investor"],
     },
     {
       name: "Wallet",
-      icon: RiHome5Line,
+      icon: RiWalletLine,
       path: `/dashboard/investor/wallet`,
       allowedRoles: ["investor"],
     },
     {
       name: "Account",
-      icon: RiHome5Line,
+      icon: RiUserLine,
       path: `/dashboard/investor/account`,
       allowedRoles: ["investor"],
     },
@@ -67,9 +69,11 @@ const Layout = ({ children }: { children: ReactNode }) => {
   const filteredItems = items.filter(
     (item) => !item.allowedRoles || item.allowedRoles.includes(role as string)
   );
+
   if (loading) {
     return <LoadingSpinner className="h-[40vh]" />;
   }
+
   return (
     <PrivateRoute>
       <DashboardLayout items={filteredItems}>{children}</DashboardLayout>
