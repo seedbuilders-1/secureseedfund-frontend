73e1020f293071e7410fdc1b22fa011f9f26267f
Abubakar-1 <abubakaruabdulkadir@gmail.com>
2024-11-21 10:20:51 +0100

    fixes

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,7 +4,6 @@ import threeImage from "../../../public/assets/images/threeImage.png";
 import NotAuthenticatedRoute from "@/route-helpers/NotAuthenticatedRoute";
 
 import { Metadata } from "next";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Sign in | Sign Up | SecureSeedFund",
@@ -19,11 +18,9 @@ export default function Auth({ children }: { children: ReactNode }) {
           <div className="w-full text-center text-[24px]">
             <p>Invest Securely-Raise Confidently</p>
           </div>
-          <Link href="/">
-            <div className="relative bottom-0 left-0 right-0 w-full cursor-pointer">
-              <Image src={threeImage} alt="logo" className="w-full" />
-            </div>
-          </Link>
+          <div className="relative bottom-0 left-0 right-0 w-full cursor-pointer">
+            <Image src={threeImage} alt="logo" className="w-full" />
+          </div>
         </div>
         <div className="lg:w-[50%] w-full">{children}</div>
       </div>
